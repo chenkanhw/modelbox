c6023d8eb1542c31afda9c6c2ce6cb897a5d6384
zxk <zhangxianku@huawei.com>
2022-05-13 10:08:48 +0800

    add armd workflow

--- docker/prepare_for_dev.sh
+++ docker/prepare_for_dev.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 VERSION_ID=$(sed -nr '/VERSION_ID/s/^VERSION_ID="(.*)"$/\1/gp' /etc/os-release)
-if [ "$VERSION_ID" == "18.04" ];then
-    ExecStar=/sbin/ldconfig
-    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/obs_3.21.8_dev-ubuntu.tar.gz
-elif [ "$VERSION_ID" == "20.04" ];then
-    ExecStar=/usr/sbin/ldconfig
-    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/cpprestsdk_2.10.15_dev.tar.gz
-    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/duktape_2.6.0_dev.tar.gz
-    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/ffmpeg_4.4_dev.tar.gz
-    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/obs_3.21.8_dev.tar.gz
-    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/opencv_4.2.0_dev.tar.gz
+if [ "$(arch)" == "x86_64" ];then
+    if [ "$VERSION_ID" == "18.04" ];then
+        ExecStar=/sbin/ldconfig
+    elif [ "$VERSION_ID" == "20.03" ];then
+        ExecStar=/usr/sbin/ldconfig
+        curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/cpprestsdk_2.10.15_dev.tar.gz
+        curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/duktape_2.6.0_dev.tar.gz
+        curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/ffmpeg_4.4_dev.tar.gz
+        curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/opencv_4.2.0_dev.tar.gz
+    fi
+    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/obssdk_3.22.3_dev.tar.gz
+    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/nlohmann-json_3.10.5.tar.gz
+    curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/Video_Codec_SDK_9.1.23.tar.gz
+elif [ "$(arch)" == "aarch64" ];then
+    if [ "$VERSION_ID" == "18.04" ];then
+        ExecStar=/sbin/ldconfig
+    elif [ "$VERSION_ID" == "20.03" ];then
+        ExecStar=/usr/sbin/ldconfig
+        curl -LJO http://download.modelbox-ai.com/third-party/aarch64/cpprestsdk_2.10.18_dev.tar.gz
+        curl -LJO http://download.modelbox-ai.com/third-party/aarch64/duktape_2.6.0_dev.tar.gz
+        curl -LJO http://download.modelbox-ai.com/third-party/aarch64/ffmpeg_4.4_dev.tar.gz
+        curl -LJO http://download.modelbox-ai.com/third-party/aarch64/opencv_4.2.0_dev.tar.gz
+    fi
+    curl -LJO http://download.modelbox-ai.com/third-party/aarch64/obssdk_3.22.3_dev.tar.gz
+    curl -LJO http://download.modelbox-ai.com/third-party/sdk/nlohmann-json_3.10.5.tar.gz
+else
+    echo "build error"
+    exit 1
 fi
-curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/nlohmann-json_3.7.3.tar.gz
-curl -LJO https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/Video_Codec_SDK_9.1.23.tar.gz
-
-cat << EOF >ldconfig.service
-[Unit]
-Description=run ldconfig
-[Service]
-type=oneshot
-ExecStar=$ExecStar
-[Install]
-WantedBy=multi-user.target
-EOF
-
-cat ldconfig.service
-chmod 755 ldconfig.service
 
 ls -lh ./
 ls -lh ./release
